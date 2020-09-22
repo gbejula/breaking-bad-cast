c7d53c7652e5085fa40c22d5f3d1a392f65b611c
Olugbenga Ajayi <gbejula@gmail.com>
2020-09-22 02:16:20 +0100

    first commit

---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import spinner from '../../img/spinner.gif'
+
+const Spinner = () => {
+  return (
+    <img 
+      src={spinner} 
+      style={{ width: '200px', display: 'block', margin: 'auto'}} 
+      alt='Loading'  
+    />
+  )
+}
+
+export default Spinner
